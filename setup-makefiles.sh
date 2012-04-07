fbb67840a37f865a4d500dbad682b64d83921fc3
Paweł Chmiel <pawel.mikolaj.chmiel@gmail.com>
2012-04-07 22:38:17 +0200

    Fix error in setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEVICE=samsung
-MANUFACTURER=cooper
+DEVICE=cooper
+MANUFACTURER=samsung
 
 mkdir -p ../../../vendor/$MANUFACTURER/$DEVICE
 
